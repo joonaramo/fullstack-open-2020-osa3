f68bdcbb8c56f4b3ca8759ceb450e54d8370e9bb
morningstreams <joona00ramo@gmail.com>
2020-12-31 13:45:33 +0200

    3.21

--- models/person.js
+++ models/person.js
@@ -19,8 +19,8 @@ const connectToDB = async () => {
 connectToDB();
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: { type: String, required: true },
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: { type: String, required: true, minlength: 8 },
 });
 
 personSchema.plugin(uniqueValidator);
